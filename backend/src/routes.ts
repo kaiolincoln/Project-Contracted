f931d09a25fbda50caf6f92d8531e85ca0e36aab
kaiolincoln <kaiolincoln2001@hotmail.com>
2026-03-07 19:34:03 -0300

    feat: endpoint: login

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,15 +2,23 @@
 
 import { Router } from "express"
 import { CreateUserController } from './controller/user/CreateUserController';
+import { AuthUserController } from "./controller/user/AuthUserController";
 import { validateSchema } from "./middlewares/validateSchema";
-import { createUserSchema } from "./schemas/userSchema";
+import { authUserSchema, createUserSchema } from "./schemas/userSchema";
 
 const router = Router();
 
 router.post(
     '/users',
     validateSchema(createUserSchema),
+    // arquivo de valicao ---- viewer, admin, editor
     new CreateUserController().handle
   );
+
+  router.post(
+    '/session', 
+    validateSchema(authUserSchema), 
+    new AuthUserController().handle
+   );
 
 export { router };
---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,50 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma/index";
+import { sign } from "jsonwebtoken";
+
+interface AuthUserServiceProps {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthUserServiceProps) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Email/Senha e obrigatorio");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Senha e obrigatorio");
+    }
+
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        subject: user.id, // ✅ CORRETO
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: token,
+    };
+  }
+}
+
+export { AuthUserService };
